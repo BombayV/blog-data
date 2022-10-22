2084be971cc582ff7eb463e15fd61c7c54884267
BombayV <OMGmsebastian@gmail.com>
2022-10-22 01:27:44 -0400

    working select + better fetching

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,28 +1,68 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
+import prompts from 'prompts';
 
-const api = 'https://random-word-api.herokuapp.com/word?length=5';
+let api = 'https://random-word-api.herokuapp.com/word?length=5';
+const fileNames = [
+    'en4.json',
+    'en5.json',
+    'en6.json',
+    'en7.json',
+    'es4.json',
+    'es5.json',
+    'es6.json',
+    'es7.json',
+]
 
-const fetchWord = async () => {
+
+
+const fetchWord = async (filePath) => {
+  // Get the lang of the file path by getting the first 2 characters
+  const lang = filePath.substring(0, 2);
+  const length = filePath.substring(2, 3);
+  if (lang === 'en') {
+    api = `https://random-word-api.herokuapp.com/word?number=5&length=${length}`;
+  } else {
+    api = `https://random-word-api.herokuapp.com/word?number=5&length=${length}&lang=${lang}`;
+  }
+
   const response = await fetch(api);
   const data = await response.json();
-  return addToFile(data[0]);
+  return addToFile(data, filePath);
 }
 
-const addToFile = (word) => {
-  // Capitalize first letter
-  const capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
-  fs.appendFile('words.txt', `"${capitalizedWord}",\n`, (err) => {
+const addToFile = (words, filePath) => {
+  let wordsToAdd = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  // Add word to json array
+  fs.readFile(`../words/${filePath}`, 'utf8', (err, data) => {
     if (err) throw err;
+    const file = JSON.parse(data);
+    // Check if one of the words is already in the file
+    wordsToAdd = wordsToAdd.filter((word) => {
+      if (!file.includes(word)) {
+        return word;
+      } else {
+        console.log(`Word "${word}" already in file`);
+      }
+    });
+    // Add the words to the file
+    const newFile = file.concat(wordsToAdd);
+    fs.writeFile(`../words/${filePath}`, JSON.stringify(newFile), (err) => {
+      if (err) throw err;
+      console.log('\x1b[32m%s\x1b[0m', `Words Added to ${filePath}`, '\x1b[0m', `${wordsToAdd}`);
+    });
   });
-  return capitalizedWord;
 }
 
-// Call the function until the program is stopped. Log the word to the console.
-setInterval(() => {
-  // Log the word to the console in yellow
-  fetchWord().then(word => console.log('\x1b[33m%s\x1b[0m\'', `New Word: ${word} | Line Count: ${fs.readFileSync('words.txt').toString().split('\n').length}`));
-}, 1500);
+(async () => {
+  const response = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Select a file',
+    choices: fileNames.map((file) => ({ title: file, value: file }))
+  });
 
-// Add a starting log to the console with green text.
-console.log('\x1b[32m%s\x1b[0m', 'Starting...');
+  setInterval(async () => {
+    await fetchWord(response.value);
+  }, 2000);
+})();
